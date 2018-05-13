1c95bbdd46744f9aae5ff47896467852126f314f
Hadrian Węgrzynowski <hadrian@hawski.com>
2018-05-13 22:09:44 +0200

    Sanitize HTML and use libc.

--- compend.c
+++ compend.c
@@ -7,31 +7,72 @@
 #include <linux/seccomp.h>
 #include <sys/prctl.h>
 #include <sys/syscall.h>
+#include <ctype.h>
+#include <time.h>
+
+
+#define ALLOWED_PATH "/hello.html"
+
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define LEN(a) (sizeof(a) / sizeof(a)[0])
 
-static const char name_hdr[] = "name=";
-static const char msg_hdr[] = "msg=";
+#define NAMEHDR "name="
+#define MSGHDR "msg="
 
+/*
 static const char template[] =
 	"<div id=\"%s\"><small>%s at %s</small>\n"
 	"<p>\n"
 	"%.*s"
 	"</div>\n";
+*/
 
-int
-main(int argc, char *argv[])
+static int
+matchstr(const char *str)
 {
-	char inbuf[4096];
-	char *in = inbuf;
-	char outbuf[8192] = "";
+	int ch;
+	do {
+		ch = getchar();
+	} while(*str && ch != EOF && ch == *str++);
+	if(ch != EOF) {
+		ungetc(ch, stdin);
+	}
+	return *str == '\0';
+}
+
+static int
+getcharx()
+{
+	int ch;
+	do {
+		ch = getchar();
+	} while(ch == '\r');
+	return ch;
+}
+
+// FIXME: proper error checking
+int
+main(void)
+{
+	char buf[BUFSIZ];
+	char fbuf[4096 * 4];
+	// FIXME: do not let overflowing of fbuf
+	int flen = 0;
+	FILE *file;
+	int ch;
+	__uint128_t random;
+	char id[17] = {0};
+	char timestr[32];
+	int inlen;
+
 	char *path = getenv("PATH_INFO");
-	if(strcmp(path, "/hello.html")) {
+	if(strcmp(path, ALLOWED_PATH)) {
 		fputs("Bad path\n", stderr);
 		syscall(SYS_exit, -1);
 		return -1;
 	}
+
 	if(strcmp(getenv("CONTENT_TYPE"), "text/plain")) {
 		fputs("Bad CONTENT_TYPE\n", stderr);
 		syscall(SYS_exit, -1);
@@ -39,66 +80,128 @@ main(int argc, char *argv[])
 	}
 	
 	chdir("..");
-	int fd = open(path+1, O_WRONLY | O_APPEND);
+	file = fdopen(open(path+1, O_WRONLY | O_APPEND), "a");
+
+	getentropy(&random, sizeof(random));
+
+	strftime(timestr, sizeof(timestr), "%F %T %z",
+		gmtime(&(time_t){time(NULL)})
+	);
 	
 	prctl(PR_SET_SECCOMP, SECCOMP_MODE_STRICT);
+	// from now on only read, write and _exit syscalls allowed
 	
-	int len = atoi(getenv("CONTENT_LENGTH"));
-	if(len > sizeof(inbuf)) {
+	inlen = atoi(getenv("CONTENT_LENGTH"));
+	if(inlen > sizeof(fbuf)) {
 		fputs("Bad CONTENT_LENGTH\n", stderr);
 		syscall(SYS_exit, -1);
 		return -1;
 	}
-	
-	len = read(STDIN_FILENO, inbuf, MIN(len, sizeof(inbuf)));
-	int rest = len;
 
-	if(rest < sizeof(name_hdr)-1) {
-		fputs("Bad name header length\n", stderr);
-		syscall(SYS_exit, -1);
-		return -1;
-	}
-	if(memcmp(inbuf, name_hdr, sizeof(name_hdr)-1)) {
+	// explicitly set buffer to avoid fstat probe
+	setbuf(stdin, buf);
+	setbuffer(file, fbuf, sizeof(fbuf));
+
+	if(!matchstr(NAMEHDR)) {
 		fputs("Bad name header\n", stderr);
 		syscall(SYS_exit, -1);
 		return -1;
 	}
-	in += sizeof(name_hdr)-1;
-	rest -= sizeof(name_hdr)-1;
-	char *nl = memchr(in, '\n', rest);
-	char *name = in;
-	*nl = '\0';
-	int name_len = nl - in;
-	in += name_len + 1;
-	rest -= name_len + 1;
 	
-	if(rest < sizeof(msg_hdr)-1) {
-		fputs("Bad msg header length\n", stderr);
-		syscall(SYS_exit, -1);
-		return -1;
+	fputs("<div id=\"", file);
+	for(int i = 0; i < 16; i++) {
+		id[i] = 'a' + ((random >> i) & 0xf);
 	}
-	if(memcmp(in, msg_hdr, sizeof(msg_hdr)-1)) {
+	fputs(id, file);
+	fputs("\">\n<small>", file);
+
+	for(ch = getcharx(); ch != '\n' && ch != EOF; ch = getcharx()) {
+		switch(ch) {
+		case '<':
+			fputs("&lt;", file);
+			break;
+		case '>':
+			fputs("&gt;", file);
+			break;
+		case '&':
+			fputs("&amp;", file);
+			break;
+		default:
+			if(iscntrl(ch)) {
+				fputc('^',  file);
+				fputc(ch + 64, file);
+			} else {
+				fputc(ch, file);
+			}
+		}
+	}
+
+	fputs(" at ", file);
+
+	fputs(timestr, file);
+
+	fputs("</small>\n<p>\n", file);
+
+	if(!matchstr(MSGHDR)) {
 		fputs("Bad msg header\n", stderr);
 		syscall(SYS_exit, -1);
 		return -1;
 	}
-	in += sizeof(msg_hdr)-1;
-	rest -= sizeof(msg_hdr)-1;
-	char *msg = in;
-	int msg_len = rest;
-	
-	int outlen = snprintf(outbuf, sizeof(outbuf), template,
-		"random", name, "today", rest, msg);
-	write(fd, outbuf, outlen);
 
-	outlen = snprintf(outbuf, sizeof(outbuf),
+	for(ch = getcharx(); ch != EOF; ch = getcharx()) {
+		if(ch == '\r') {
+			continue;
+		}
+		switch(ch) {
+		case '<':
+			fputs("&lt;", file);
+			break;
+		case '>':
+			fputs("&gt;", file);
+			break;
+		case '&':
+			fputs("&amp;", file);
+			break;
+		case '\n':
+			ch = getcharx();
+			if(ch != '\n') {
+				fputs("<br>\n", file);
+			} else {
+				do {
+					ch = getcharx();
+				} while(ch == '\n');
+				if(ch != EOF) {
+					fputs("\n<p>\n", file);
+				} else {
+					fputc('\n', file);
+				}
+			}
+			ungetc(ch, stdin);
+			break;
+		default:
+			if(iscntrl(ch)) {
+				fputc('^',  file);
+				fputc(ch + 64, file);
+			} else {
+				fputc(ch, file);
+			}
+		}
+	}
+
+	fputs("</div>\n", file);
+
+	fflush(file);
+
+	setbuf(stdout, buf);
+	printf(
 		"HTTP/1.0 302 Ok\r\n"
 		"Status: 302 Moved\r\n"
-		"Location: %s\r\n"
+		"Location: %s#%s\r\n"
 		"\r\n",
-		path
+		path, id
 	);
-	write(STDOUT_FILENO, outbuf, outlen);
+	fflush(stdout);
+
 	syscall(SYS_exit, 0);
 	return 0;
 }
