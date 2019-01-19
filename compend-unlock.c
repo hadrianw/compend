a18f62e9594c165e7e1af09795e38cdf9432a361
Hadrian Węgrzynowski <hadrian@hawski.com>
2019-01-19 23:13:27 +0100

    Implement first semi-complete version of unlock.

--- compend-unlock.c
+++ compend-unlock.c
@@ -1,47 +1,74 @@
-WIP pseudocode
+#include <unistd.h>
+#include <fcntl.h>
 
+// Linux only: uses sendfile
 int
-append(char *src, int pos, char *dst)
+copy(int src, off_t off, int dst)
 {
-	s = open(src)
-	d = open(dst, O_APPEND)
-	lseek(s, pos, SEEK_POS)
+	struct stat s_stat;
+	fstat(src, &s_stat);
 	
+	if(s_stat.st_size < off) {
+		return -1;
+	}
+	size_t count = s_stat.st_size - off;
+	ssize_t size = 0;
+
+	for(; size >= 0 && count > 0; count -= size) {
+		size = sendfile(dst, src, &off, count);
+	}
+
+	/* or something like this
+
+	lseek(src, off, SEEK_POS);
 	do {
-		size = sendfile(d, s, &pos, whole_size)
-	} while(size > 0);
-
-	/*or
-
-	read(s, buf)
-	write(d, buf)
+		size = read(src, buf, sizeof buf);
+		write(dst, buf, size);
+	} while();
 	*/
 }
 
-wait_until_write_closed(file)
+// Linux only: uses fcntl F_SETLEASE
+int
+wait_until_write_closed(char *path)
 {
-	// Linux only
-	fd = open(file, rdonly)
-	while(fcntl(fd, F_SETLEASE, F_RDLCK) < 0) {
-		usleep()
+	int fd = open(file, O_RDONLY);
+	while(fcntl(fd, F_SETLEASE, F_RDLCK) < 0 && errno == EAGAIN) {
+		usleep(500 * 1000)
 	}
-	close(fd)
+	// don't care about the return
+	close(fd);
+	return 0;
 }
 
 int
 main(int argc, char *argv[])
 {
-	append(post, orig.st_size, post.edit)
-	exec(cp post.edit post.orig)
+	int live = open("post", O_RDONLY);
+	int orig = open("post.orig", O_RDONLY);
+	int edit = open("post.edit", O_APPEND);
+	int inflight;
 
-	link(post, post.inflight)
-	rename(post.edit, post)
+	struct stat o_stat;
+	fstat(orig, &o_stat);
+	copy(live, o_stat.st_size, edit);
 
-	wait_until_write_closed(post.inflight)
+	rename("post.edit", "post.orig");
 
-	append(post.inflight, orig.st_size, post)
+	link("post", "post.inflight");
+	inflight = live;
 
-	unlink(post.inflight)
+	struct stat e_stat;
+	fstat(edit, &e_stat);
+	rename("post.edit", "post");
+	live = edit;
+
+	wait_until_write_closed(inflight);
+
+	//append(post.inflight, orig.st_size, post)
+	copy(inflight, e_stat.st_size, live);
+
+	unlink("post.inflight");
 	
 	return 0;
 }
